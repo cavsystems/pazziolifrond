f6e346544e4aebc06fd949e478abf4a318ed7615
cavsystems <desarrollo@cavsystmes.com.co>
2025-09-22 10:17:29 -0500

    cambiosfrond

---
+++ src/environments/enviroment.pro.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  vendedor: 'token_vendedor',
+  api: 'https://pazzioliweb.cavsystems.com.co/api',
+  apisocket: 'https://pazzioliweb.cavsystems.com.co',
+};
