c44b9640c33d08930be2f49b19f1d2c89a7f2f16
cavsystems <desarrollo@cavsystmes.com.co>
2025-12-15 16:18:12 -0500

    cambiosfrondtomapedidos

--- src/app/plantillas/administracion/componentes/cuadre/cuadre.component.ts
+++ src/app/plantillas/administracion/componentes/cuadre/cuadre.component.ts
@@ -447,7 +447,7 @@ this.reciboscuadre=dat.datosaux[0]
                this.targetas+=Number(this.ventascuadre.ftDebito)+Number(this.ventascuadre.ftCredito)+Number(this.reciboscuadre.TDebito)+Number(this.reciboscuadre.TCredito)
                this.otros+=Number(this.ventascuadre.ftBono)+Number(this.ventascuadre.ftCheque)+Number(this.reciboscuadre.TCheque)
                this.totalglobalmetodos+=  this.efectivoglobal+this.targetas
-               this.totalcaja+=(Number(this.ventascuadre.totalVentas)+Number(this.reciboscuadre.totalRecibos))-(Number(
+               this.totalcaja+=(Number(this.ventascuadre.totalVentas))-(Number(
                 this.egresoscuadre.tEgresos
                )+Number(this.devolucionescuadre.totalDevolucion))
                  console.log(this.ventascuadre.totalVentas,this.reciboscuadre.totalRecibos,this.egresoscuadre.tEgresos,this.devolucionescuadre.totalDevolucion)
